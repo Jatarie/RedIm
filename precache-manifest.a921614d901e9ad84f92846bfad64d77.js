3f84f23ff2acd465579d41447337eaa8e4d1bed8
Matthew Smyth <matthew.e.j.smyth@gmail.com>
2021-05-16 12:01:12 +0100

    Updates

---
+++ precache-manifest.a921614d901e9ad84f92846bfad64d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69a698f98833edd6c4a5",
+    "url": "/RedIm/static/css/main.16ea36be.chunk.css"
+  },
+  {
+    "revision": "69a698f98833edd6c4a5",
+    "url": "/RedIm/static/js/main.69a698f9.chunk.js"
+  },
+  {
+    "revision": "15a3732928f1acb7eb01",
+    "url": "/RedIm/static/js/1.15a37329.chunk.js"
+  },
+  {
+    "revision": "1f92ae9157fc1e05313e",
+    "url": "/RedIm/static/js/runtime~main.1f92ae91.js"
+  },
+  {
+    "revision": "3dbe813b333afee69c5df057e44e1f4a",
+    "url": "/RedIm/index.html"
+  }
+];
